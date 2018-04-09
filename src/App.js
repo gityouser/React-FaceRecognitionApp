c17bf239e8bd2535aa358ca06be97e19d5bc53ae
Andrei Constantinescu <andreiconstantinescumail@gmail.com>
2018-04-09 23:14:34 +0300

    Created visuals, no functionality.

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import Particles from 'react-particles-js';
+import Navigation from './components/Navigation/Navigation';
+import Logo from './components/Logo/Logo';
+import Rank from './components/Rank/Rank';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+
+import './App.css';
+
+const particlesOptions = {
+  particles: {
+    number: {
+      value: 30,
+      density: {
+        enable: true,
+        value_area: 300
+      }
+    }
+  }
+}
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Particles className='particles'
+              params={particlesOptions}
+            />
+        <Navigation />
+        <Logo />
+        <Rank />
+        <ImageLinkForm />
+        {
+          // <FaceRecognition />
+        }
+      </div>
+    );
+  }
+}
+
+export default App;
