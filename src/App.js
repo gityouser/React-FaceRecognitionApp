47411aacc44117c5db92669fcf3455a0eee34f1d
Andrei Constantinescu <andreiconstantinescumail@gmail.com>
2018-05-03 20:23:40 +0300

    Backend integration in progress

--- src/App.js
+++ src/App.js
@@ -35,8 +35,26 @@ class App extends Component {
       imageUrl: '',
       box: {},
       route: 'signin',
-      isSignedIn: false
+      isSignedIn: false,
+      user: {
+        id: '',
+        name: '',
+        password: '',
+        email: '',
+        entries: 0,
+        joined: ''
+      }
     }
+  }
+
+  loadUser = (data) => {
+    this.setState({user: {
+      id: data.id,
+      name: data.name,
+      email: data.email,
+      entries: data.entries,
+      joined: data.joined
+    }})
   }
 
   calculateFaceLocation = (data) => {
@@ -99,7 +117,7 @@ class App extends Component {
         : (
           route === 'signin'
           ? <Signin onRouteChange={this.onRouteChange}/>
-          : <Register onRouteChange={this.onRouteChange}/>
+          : <Register onRouteChange={this.onRouteChange} loadUser={this.loadUser}/>
         )
 
 
